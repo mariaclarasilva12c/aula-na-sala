c966849c16cf3154b08ec86f53489341ef514769
EA - Maria Clara Silva Melo Dos Santos <maria_melo01@educadventista.org>
2026-03-10 12:34:28 -0300

    fv

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import express from "express"
+import express, { request } from "express"
 const servidor = express();
+
+
+servidor.get ("/"(request,response) => {
+
+
+
+
+}
+
+
+)
 
 servidor.listen(3000, () => {
     console.log("O servidor está ligado!")
